f837b55fcb74f5c7eaeab6c6c621ffb3be7e757f
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2021-11-29 23:13:39 +0100

    utilisation de variables pour chaque produit avec URLSearchParams (étape 4 : faire le lien entre un produit de la page d'accueil et la page Produit)

--- back/models/Product.js
+++ back/models/Product.js
@@ -85,6 +85,60 @@ exports.findById = (id) => {
   );
 }
 
-
-
-
+/** The using of "URLSearchParams". */
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=107fb5b75607497b96722bda5b504926";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=415b7cacb65d43b2b5c1ff70f3393ad1";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=055743915a544fde83cfdfc904935ee7";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=a557292fe5814ea2b15c6ef4bd73ed83";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=8906dfda133f4c20a9d0e34f18adcf06";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=77711f0e466b4ddf953f677d30b0efc9";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=034707184e8e4eefb46400b5a3774b5f";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
+var str = "https://audrey-hub-dev.github.io/projet5/front/html/product.html?id=a6ec5b49bd164d7fbe10f37b6363f9fb";
+var url = new URL(str);
+var search_params = new URLSearchParams(url.search); 
+if(search_params.has('id')) {
+  var id = search_params.get('id');
+  console.log(id)
+}
