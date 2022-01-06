ec2034370db618a77379c082a467f0921b6ec127
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-01-06 20:58:35 +0100

    modifications récupération des valeurs du formulaire, suppression de la class, essaie envoie données fetch

--- front/js/logic/cart.js
+++ front/js/logic/cart.js
@@ -6,20 +6,20 @@ function changeQ() {
     for (let l = 0; l < numberDisplay.length; l++) {//création d'une recherche des quantités dans le panier avec une boucle 
         numberDisplay[l].addEventListener('change', () => {
             //je crée la variable pour parcourir les quantités dans le tableau 
-            let changeDisplay = productArray[l].useQuantity;
+            let changeDisplay = cart[l].useQuantity;
             //je crée la variable pour afficher la valeur numérique 
             let numberDisplayResult = numberDisplay[l].valueAsNumber;
             //je crée une variable qui montre les résultats de la recherche dans la tableau
             /* dans le tableau, recherche d'un produit avec sa valeur numérique qui doit être strictement inégale
             à la quantité déjà inscrite 
             */
-            const resultFind = productArray.find((product) => product.numberDisplayResult !== changeDisplay);
+            const resultFind = cart.find((product) => product.numberDisplayResult !== changeDisplay);
            
              // la quantité modifiée du produit est affichée dans l'emplacement prévu
             resultFind.useQuantity = numberDisplayResult;
     
-            productArray[l].useQuantity = resultFind.useQuantity;
-            localStorage.setItem("cart", JSON.stringify(productArray));
+            cart[l].useQuantity = resultFind.useQuantity;
+            localStorage.setItem("cart", JSON.stringify(cart));
 
         })
     }
@@ -36,25 +36,81 @@ changeQ();
 const orderBtn = document.querySelector("#order");
 
 //écoute du bouton order
-orderBtn.addEventListener("click", () => {
+orderBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
 
 
 // création d'une classe pour mettre l'objet dans lequel vont les valeurs du formulaire
-class form{
-    constructor(){
+/*class form{
+    constructor(input){
     //constructor est une méthode pour utiliser et initialiser un objet lorsqu'on utilise le mot clé class
     this.firstName = document.querySelector("#firstName").value;
     this.lastName = document.querySelector("#lastName").value;
     this.address = document.querySelector("#address").value;
     this.city = document.querySelector("#city").value;
-    this.email = document.querySelector("#email").value
-    
-
-}}
+    this.email = document.querySelector("#email").value;
+    this.input = document.querySelector(`#${input}`).value
+    }
+}
 
 //appel de l'instance de class form pour créer l'objet formValues
 const formValues = new form();
 console.log(formValues)
+console.log("formValues")
+*/
+
+//récupération des valeurs du formulaire pour les mettre dans le localStorage
+/*localStorage.setItem("firstName", document.querySelector("#firstName").value);
+localStorage.setItem("lastName", document.querySelector("#lastName").value);
+localStorage.setItem("address", document.querySelector("#address").value);
+localStorage.setItem("city", document.querySelector("#city").value);
+localStorage.setItem("email", document.querySelector("#email").value);
+
+
+//mettre les valeurs du formulaire dans un objet 
+const form = {
+    firstName: localStorage.getItem("firstName"),
+    lastName: localStorage.getItem("lastName"),
+    address: localStorage.getItem("address"),
+    city: localStorage.getItem("city"),
+    email : localStorage.getItem("email"),
+}
+console.log("form")
+console.log(form)
+*/
+
+
+//récupération des valeurs du formulaire 
+const formValues = {
+    firstName: document.querySelector("#firstName").value,
+    lastName: document.querySelector("#lastName").value,
+    address: document.querySelector("#address").value,
+    city: document.querySelector("#city").value,
+    email: document.querySelector("#email").value,
+}
+console.log("formValues")
+console.log(formValues)
+
+//mettre l'objet "formValues" dans le localStorage
+//localStorage.setItem("formValues", JSON.stringify(formValues))
+
+
+
+
+//mettre les valeurs du formulaire et les produits sélectionnés dans un objet à envoyer au serveur
+/*const sendForm = {
+    cart, 
+    formValues
+}
+console.log("sendForm")
+console.log(sendForm)
+*/
+
+
+
+
+
 
 
 //gestion des expressions régulières et de la sauvegarde du localStorage
@@ -178,21 +234,63 @@ if (controlFirstName() && controlLastName() && controlAddress() && controlCity()
     alert("veuillez renseigner correctement les champs")
     
 };
-    
+
+//fin de la gestion des expressions régulières et de la sauvegarde du localStorage
+
 //mettre les valeurs du formulaire et les produits dans un objet à envoyer au serveur
-const formProduct = {
+/*const sendForm = {
+    cart,
     formValues,
-    productArray
-}
-console.log(formProduct)
+};
+console.log("sendForm");
+*/
 
-}); 
 
-//fin écoute du bouton 
+//mettre les valeurs du formulaire et les produits sélectionnés dans un objet à envoyer au serveur
+const order = { 
+    cart, //tableau de produits
+    formValues //objet de contact 
+};
+console.log("order")
+console.log(order)
+
+
+const options = {
+    method: 'POST',
+    body: JSON.stringify(order),
+    headers: {
+        "Content-Type": "application/json" 
+    },
+};
+
+
+//envoie de l'objet sendForm au serveur
+fetch('http://localhost:3000/api/products/order' , options )
+.then((response) => response.json())
+.then((response) => console.log(response)); 
+
+
+   
+
+
+
+
+   
+   
+
+
+
+
+
+
+}); //fin écoute du bouton 
+
+
+
 
 
 //sauvegarder les champs du formulaire 
-//chercher la key dans le loaclStorage et la mettre dans une variable
+//chercher la key dans le localStorage et la mettre dans une variable
 const dataForm = localStorage.getItem("formValues");
 const dataFormArray = JSON.parse(dataForm);
 
@@ -203,7 +301,8 @@ const dataFormArray = JSON.parse(dataForm);
 //fonction pour sauvegarder les données du formulaire 
 function saveForm(input) {
     //condition pour ne pas avoir de problème dans la console si le formulaire est vide
-    if (dataFormArray == null){     
+    if (dataFormArray == null){   
+        
     }else{
     document.querySelector(`#${input}`).value = dataFormArray[input]; 
     }
