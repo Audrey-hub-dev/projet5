655e44da8dbcbd3b47625be088418dc480608cd1
Audrey-hub-dev <81632442+Audrey-hub-dev@users.noreply.github.com>
2022-01-20 21:30:23 +0100

    essais pour modifier la fonction de modification des quantités

--- front/js/logic/cart.js
+++ front/js/logic/cart.js
@@ -1,14 +1,175 @@
 // modification d'une quantité dans le panier
 
+/*function changeQ() {
+    //je sélectionne l'élément quantité que l'on peut modifier
+   var numberDisplay = document.querySelectorAll(".itemQuantity") ;
+    console.log(numberDisplay)
+    
+
+ 
+    //let numberDisplay = document.getElementsByClassName("itemQuantity") ;
+    //je crée la variable pour parcourir les quantités dans le tableau 
+    
+   
+
+    for (let i in numberDisplay) {
+      
+
+   //création d'une recherche des quantités dans le panier avec une boucle 
+        addEventListener('change', (event) => {
+            event.preventDefault();
+            
+            
+            let numberDisplay = document.querySelectorAll(".itemQuantity") ;
+    console.log(numberDisplay)
+           
+            let changeDisplay = cartArray[i].numberDisplay[i];
+            //je crée la variable pour afficher la valeur numérique 
+            let numberDisplayResult = numberDisplay[i].valueAsNumber;
+            //je crée une variable qui montre les résultats de la recherche dans la tableau
+            /* dans le tableau, recherche d'un produit avec sa valeur numérique qui doit être strictement inégale
+            à la quantité déjà inscrite 
+            */
+        
+
+
+           /*const resultFind = cartArray.find((product) =>
+            
+            
+            
+            
+            product.numberDisplayResult
+            
+            
+            !== changeDisplay);
+           
+           
+             // la quantité modifiée du produit est affichée dans l'emplacement prévu
+          resultFind.numberDisplay = numberDisplayResult
+    
+            cartArray[i].useQuantity = resultFind.useQuantity;
+            
+            
+            localStorage.setItem("cart", JSON.stringify(cartArray));
+
+        })
+    
+}   }   
+changeQ();
+
+*/
+
+//function changeQ() {
+
+//var numberDisplay = document.querySelectorAll(".itemQuantity")
+
+//for (var i = 0; i < numberDisplay.length; i++) 
+//numberDisplay[i].addEventListener("change", function(event){
+
+//var quantityMod = document.querySelectorAll(".itemQuantity")
+
+//for (var i = 0; i < cartArray.length; i++) 
+   
+
+    /*var quantityMod = document.querySelectorAll(".itemQuantity")
+    var quantity = document.getElementById("quantity");
+    var optionQuantity = quantity
+    var useQuantity = optionQuantity
+    let cartArray = JSON.parse(localStorage.getItem("cart")); 
+for (var i=0; i < quantityMod.length; i++) {
+   
+    quantityMod[i].addEventListener("change", function(event){
+        
+        var qqMod = useQuantity
+
+        var quantityValue = quantityMod[i]
+
+//console.log(quantity)
+
+const resultFind = cartArray.find((el) => el.quantityValue !== qqMod);
+
+       resultFind.useQuantity = quantityValue    
+            cartArray[i].useQuantity = resultFind.useQuantity;
+    
+localStorage.setItem("cart", JSON.stringify(cartArray));
+
+
+    
+ }
+ 
+    )}
+}
+
+changeQ()
+*/
+
+
+
+/*let quantityMod = document.querySelectorAll('.itemQuantity');
+let i = quantityMod.length;
+
+
+    
+  
+
+function changeQ() {
+    quantityMod[i].addEventListener("change", changeQ => {
+       
+let qqMod = cartArray[i].useQuantity
+let quantityValue = quantityMod[i].value
+    const resultFind = cartArray.find((el) => el.quantityValue !== qqMod);
+
+    resultFind.useQuantity = quantityValue    
+         cartArray[i].useQuantity = resultFind.useQuantity;
+ 
+         localStorage.setItem("cart", JSON.stringify(cartArray));
+
+
+
+}
+    )}
+
+    changeQ()
+
+*/
+
+
+/*let quantityMod = document.querySelectorAll('.itemQuantity');
+document.getElementsByName("itemQuantity") [0].addEventListener('change',modQuantity );
+ 
+function modQuantity() {
+    for (var i = 0; i < quantityMod.length; i++)   {    
+let qqMod = cartArray.useQuantity
+let quantityValue = quantityMod.value
+    if (resultFind = cartArray.find((el) => el.quantityValue !== qqMod)) {
+
+    resultFind.useQuantity = quantityValue    
+         cartArray.useQuantity = resultFind.useQuantity;
+ 
+         localStorage.setItem("cart", JSON.stringify(cartArray));
+
+
+}else{
+    localStorage.setItem("cart", JSON.stringify(cartArray));
+}
+
+
+
+
+
+}
+}
+*/
+
 function changeQ() {
     //je sélectionne l'élément quantité que l'on peut modifier
     let numberDisplay = document.querySelectorAll(".itemQuantity");
-    for (let l = 0; l < numberDisplay.length; l++) {//création d'une recherche des quantités dans le panier avec une boucle 
-        numberDisplay[l].addEventListener('change', () => {
+    for (let i = 0; i < numberDisplay.length; i++) {//création d'une recherche des quantités dans le panier avec une boucle 
+        numberDisplay[i].addEventListener('change', () => {
             //je crée la variable pour parcourir les quantités dans le tableau 
-            let changeDisplay = cart[l].useQuantity;
+            let changeDisplay = cart[i].useQuantity;
             //je crée la variable pour afficher la valeur numérique 
-            let numberDisplayResult = numberDisplay[l].valueAsNumber;
+            let numberDisplayResult = numberDisplay[i].valueAsNumber;
             //je crée une variable qui montre les résultats de la recherche dans la tableau
             /* dans le tableau, recherche d'un produit avec sa valeur numérique qui doit être strictement inégale
             à la quantité déjà inscrite 
@@ -18,7 +179,7 @@ function changeQ() {
              // la quantité modifiée du produit est affichée dans l'emplacement prévu
             resultFind.useQuantity = numberDisplayResult;
     
-            cart[l].useQuantity = resultFind.useQuantity;
+            cart[i].useQuantity = resultFind.useQuantity;
             localStorage.setItem("cart", JSON.stringify(cart));
 
         })
@@ -27,7 +188,234 @@ function changeQ() {
 changeQ();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*function qq() {
+
+let numberDisplay = document.querySelectorAll(".itemQuantity") ;
+    console.log(numberDisplay)
+
+
+    for ( let i=0; i< numberDisplay.length; i++) {
+
+        //création d'une recherche des quantités dans le panier avec une boucle 
+            
+             numberDisplay[i].addEventListener('change',() => {
+             
+
+
+
+                let changeDisplay = cartArray[i].useQuantity;
+                //je crée la variable pour afficher la valeur numérique 
+                let numberDisplayResult = numberDisplay[i].valueAsNumber;
+                //je crée une variable qui montre les résultats de la recherche dans la tableau
+                /* dans le tableau, recherche d'un produit avec sa valeur numérique qui doit être strictement inégale
+                à la quantité déjà inscrite 
+                */
+            
+        
+        
+                /*let resultFind = cartArray.find((product) =>
+                
+                
+                
+                
+                product.numberDisplayResult
+                
+                
+                !== changeDisplay);
+               
+                
+               
+                 // la quantité modifiée du produit est affichée dans l'emplacement prévu
+                numberDisplayResult = resultFind.numberDisplay
+        
+                cartArray[i].useQuantity = resultFind.useQuantity;
+                
+                
+                localStorage.setItem("cart", JSON.stringify(cartArray));
+        
+            })
+
+
+
+
+             }
+                
+            }
+
+     qq()
+        
+*/
+
+
+
+
+
+    
+
+
+
+
+  
+
+
+
+
+
+
+
+/*
+// condition si déjà présent (if) ou non (else)
+  
+if (cartArray) {// si dans le panier il y a déjà un produit 
+    let resultFind = //création variable de la recherche d'un résultat dans le panier 
+    cartArray.find ( //find permet de chercher un élément sur un tableau par rapport à une condition 
+    (product) => product.id === productId && product.useColor === optionColor);
+    /*si dans le panier, l'id du produit est égal à l'id du produit que l'on souhaite ajouter
+    et que la couleur du produit est égal à la couleur du produit que l'on souhaite ajouter
+    alors find retourne l'élément en question*/
+     /* if(resultFind) { // si il trouve un produit même couleur même id 
+        resultFind.useQuantity++;// il ajoute une quantité à celle déjà présente 
+        localStorage.setItem("cart", JSON.stringify(cartArray));
+  
+      }else if (resultFind === undefined) { //si il n'y a pas de produit même couleur avec même id dans le panier 
+  
+        //newProduct.useQuantity = 1 // il crée une quantité 
+        addProductLocalStorage(); 
+    }
+  }
+  else { //sinon le panier est vide 
+      cartArray = []
+      addProductLocalStorage(); 
+      }
+    }
+      )}
+   
+  addToCart(productData)/*paramètre product Data car on récupère ce paramètre une fois qu'il est passé dans ce productDisplay, on ne veut pas récupérer productData vide*/
+  
+
+
+
+
+
+
+
+
+
+/*
+
 //suppression d'une quantité dans le panier 
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+
+}else {
+    ready()
+
+}
+
+
+function ready() {
+
+var removeCartItem = document.getElementsByClassName('deleteItem')
+console.log(removeCartItem)
+
+for (var i=0; i< removeCartItem.length; i++) {
+    var button = removeCartItem[i]
+    button.addEventListener('click', removeCartItem) 
+       
+    }    
+
+var inputItemQuantity = document.getElementsByClassName('itemQuantity')
+    
+    for (var i=0 ; i < inputItemQuantity.length; i++) {
+        var input = inputItemQuantity[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+}
+
+
+
+function removeCartItem(event) {
+    
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
+    updateCartTotal()
+
+
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+
+    }
+    updateCartTotal()
+}
+
+
+
+let section = document.querySelector(".cart");
+section.setAttribute('id', 'idCart'); 
+
+
+function updateCartTotal() {
+    var cartContainer = document.querySelector('.cart')
+    var cartItemsContainer = cartContainer.querySelector('#cart__items')
+    console.log(cartItemsContainer)
+    var total = 0
+    for (var i = 0; i < cartItemsContainer.length; i++) {
+        var cartItems = cartItemsContainer[i]
+        var priceElement = cartItems.getElementsByClassName('cart__item__content__titlePrice') [0]
+       console.log(priceElement)
+        var quantityElement = cartItems.getElementsByClassName('cart__item__content__settings__quantity') [0]
+        console.log(quantityElement)
+
+        var price = parseFloat(priceElement.innertext.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+        console.log(price * quantity)
+    }
+    
+    document.getElementsByClassName('cart__price')[0].innerHTML = '$' + total
+    
+}
+
+
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 
 
 // étape 10 : passer la commande
@@ -114,7 +502,7 @@ function controlAddress() {
     if (regExAddress(addressRegex)) {/*soit donc un ensemble quelconque de chiffre suivit éventuellement d'un espace
         suivit d'un ensemble quelconque de lettres espaces virgules ou points suivit éventuellement d'un espace suivit d'une suite de 5 chiffres suivit éventuellement d'un espace suivit d'une ensemble quelconque de lettres
         */
-        addressErrorMsg.innerHTML = "correct"
+      /*  addressErrorMsg.innerHTML = "correct"
         console.log("ok")
         return true
 
@@ -180,8 +568,8 @@ const order = {
     cart, //tableau de produits
     formValues //objet de contact 
 };
-console.log("order")
-console.log(order)
+
+console.log("order" + order)
 
 
 const options = {
@@ -234,4 +622,4 @@ console.log("dataFormArray");
 
 
     
-  
+*/
